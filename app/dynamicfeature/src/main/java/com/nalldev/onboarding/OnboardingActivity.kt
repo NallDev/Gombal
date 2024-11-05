c8596ad7c0c9d4f15f0eb4bee4ff318dec1d64a9
afrinaldiGenzo <afrinaldi190401@gmail.com>
2024-11-06 01:32:37 +0700

    set to java 11 and support library

--- app/dynamicfeature/src/main/java/com/nalldev/onboarding/OnboardingActivity.kt
+++ app/dynamicfeature/src/main/java/com/nalldev/onboarding/OnboardingActivity.kt
@@ -1,11 +1,7 @@
 package com.nalldev.onboarding
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.nalldev.onboarding.databinding.ActivityOnboardingBinding
 
 class OnboardingActivity : AppCompatActivity() {
@@ -14,32 +10,8 @@ class OnboardingActivity : AppCompatActivity() {
         ActivityOnboardingBinding.inflate(layoutInflater)
     }
 
-    private val viewModel by viewModels<OnBoardingViewModel>()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(binding.root)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-
-        viewModel.page.observe(this) { page ->
-            if (page == 2) {
-                runOnUiThread {
-                    binding.main.transitionToEnd()
-                }
-            } else if (page == 3) {
-                runOnUiThread {
-                    binding.main.transitionToState(R.id.three)
-                }
-            }
-        }
-
-        binding.btnNext.setOnClickListener {
-            viewModel.nextPage()
-        }
     }
 }
